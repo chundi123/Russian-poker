7ad562a9f48bb7bf3e178d869de243c156127005
ajaykumar <welcome@123>
2025-12-23 10:07:04 +0530

    added quartz and lombok

--- src/main/java/com/demo/tournament/TournamentApplication.java
+++ src/main/java/com/demo/tournament/TournamentApplication.java
@@ -3,8 +3,12 @@ package com.demo.tournament;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class TournamentApplication {
     public static void main(String[] args) {
         SpringApplication.run(TournamentApplication.class, args);
